eb6da57e1855be8fd5f89304a86759661bfd97d0
Julian Montague <thatoneprogrammerkid@gmail.com>
2015-12-31 21:50:29 +0100

    Change format of block types table

--- block.lua
+++ block.lua
@@ -1,26 +1,27 @@
 local block = {}
 
-local stone =
+block.type =
 {
-  name = "Stone",
-  color = game.color.new( 204, 204, 204 ),
-  image = nil
+  ["stone"] =
+  {
+    name = "Stone",
+    color = game.color.new( 204, 204, 204 ),
+    image = nil
+  },
+  ["log"] =
+  {
+    name = "Log",
+    color = game.color.new( 114, 95, 6 ),
+    image = nil
+  },
+  ["planks"] =
+  {
+    name = "Wooden Planks",
+    color = game.color.new( 180, 150, 10 ),
+    image = nil
+  }
 }
 
-local wood =
-{
-  name = "Log",
-  color = game.color.new( 114, 95, 6 ),
-  image = nil
-}
-
-local wooden_planks = 
-{
-  name = "Wooden Planks",
-  color = game.color.new( 180, 150, 10 ),
-  image = nil
-}
-
-block.type = { stone, wood, wooden_planks }
+block.order = { "stone", "log", "planks" }
 
 return block
