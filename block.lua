739ddbe8d27d77ac24b457eb3c568e96c17e6c32
Julian Montague <thatoneprogrammerkid@gmail.com>
2016-01-06 10:59:00 -0600

    Move block order to type table
    block.order had the names of the blocks in their order, but this seemed unecessary so I removed block.order and simply added an 'id' key to each block's table.

--- block.lua
+++ block.lua
@@ -5,23 +5,24 @@ block.type =
   ["stone"] =
   {
     name = "Stone",
+    id = 1,
     color = game.color.new( 204, 204, 204 ),
     image = nil
   },
   ["log"] =
   {
     name = "Log",
+    id = 2,
     color = game.color.new( 114, 95, 6 ),
     image = nil
   },
   ["planks"] =
   {
     name = "Wooden Planks",
+    id = 3,
     color = game.color.new( 180, 150, 10 ),
     image = nil
   }
 }
-
-block.order = { "stone", "log", "planks" }
 
 return block
