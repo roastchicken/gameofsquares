a63480c27e8adc4f6ceaad40c27751b6814e6448
roastchicken <roast@thatoneprogrammerkid.com>
2016-02-15 09:09:16 -0600

    Remove blocks' color property

--- block.lua
+++ block.lua
@@ -8,28 +8,24 @@ block.type =
   {
     name = "Stone",
     id = 1,
-    color = Color( 204, 204, 204 ),
     image = { 0, 0 }
   },
   ["log"] =
   {
     name = "Log",
     id = 2,
-    color = Color( 114, 95, 6 ),
     image = { 1, 0 }
   },
   ["planks"] =
   {
     name = "Wooden Planks",
     id = 3,
-    color = Color( 180, 150, 10 ),
     image = { 0, 1 }
   },
   ["leaves"] =
   {
     name = "Leaves",
     id = 4,
-    color = Color( 10, 171, 40 ),
     image = { 1, 1 }
   }
 }
